63edb89e57a09e795430b88e269f3def1c0cc6e9
shixl <xlshiz@users.noreply.github.com>
2022-06-27 23:31:53 +0800

    Add toki-tabs

--- modules/editor/tabs/packages.el
+++ modules/editor/tabs/packages.el
@@ -1,5 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; editor/tabs/packages.el
 
-(package! awesome-tab :recipe (:host github :repo "manateelazycat/awesome-tab" :files ("*.el") :build (autoloads)) :pin "1dc909488f893d1fe8bb113120da4b47b7d3fc39")
+(package! awesome-tab :recipe (:host github :repo "xlshiz/awesome-tab" :files ("*.el") :build (autoloads)))
 (package! sort-tab :recipe (:host github :repo "manateelazycat/sort-tab" :files ("*.el") :build (autoloads)))
+(package! toki-tabs :recipe (:host github :repo "xlshiz/toki-tabs.el" :files ("*.el") :build (autoloads)))
