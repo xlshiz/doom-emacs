d6a1e8861b2c35ea6f7d13f54008269f254bb789
shixl <xlshiz@users.noreply.github.com>
2023-08-27 00:04:06 +0800

    Sync 7a7503045850ea83f205de6e71e6d886187f4a22
    revert: fix(cli): set LC_ALL to force english output
    
        LC_ALL must be set to a locale that exists on the current system, and
        there is no locale I can safely expect all systems to have (see #7361).
        Fortunately, c8070f1 seems to have addressed the underlying issue, so
        this fix can be reverted.
    
        Fix: #7361
        Revert: #7331
        Revert: 150ccd630565
        Ref: c8070f11a448

--- modules/editor/hl/packages.el
+++ modules/editor/hl/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; editor/hl/packages.el
 
-(package! hl-todo :pin "a627d33214e60fc5a2fe6164411d908e1afdba59")
+(package! hl-todo :pin "0faf8569b67f5b23891416d9e7a67e3843338f2a")
 (if (modulep! +diff-hl)
-    (package! diff-hl :pin "d20f16bf5eadd66e775f215e800f25caddae8cb5")
+    (package! diff-hl :pin "b5651f1c57b42e0f38e01a8fc8c7df9bc76d5d38")
   (package! git-gutter-fringe :pin "648cb5b57faec55711803cdc9434e55a733c3eba"))
